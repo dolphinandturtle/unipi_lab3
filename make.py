ad145885dd7b86a90ec538e75ceb05a13f2e9f02
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-08-31 05:11:53 +0200

    lowpass vcvs

--- make.py
+++ make.py
@@ -1,5 +1,14 @@
 import os
+import sys
 from subprocess import call
 
 
-call(["pdflatex", os.path.join("src", "main.tex")])
+if len(sys.argv) < 2:
+    call(["pdflatex", os.path.join("src", "main.tex")])
+
+elif sys.argv[1] == "clean":
+    call(["rm", "-f", "main.aux"])
+    call(["rm", "-f", "main.log"])
+    call(["rm", "-f", "main.pdf"])
+
+
